2f248d0ac4fbeeb40253e860e660a47c665b8829
Pavel Lebedev <p.lebedev8888@gmail.com>
2025-03-22 19:05:43 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван"; // Имя
+        post.surname = "Иванов"; // Фамилия
+        post.patronymic = "Иванович"; // Отчество
+        post.telephone = "+7(900)000-00-00"; // номер телефона
+        post.passport = "0000 №000000"; // номер паспорта
+        post.subscription = "true"; // наличие подписки
+        post.birthday = new FormDate();
+        post.birthday.day = 13; // день рождения
+        post.birthday.month = 1; // месяц рождения
+        post.birthday.year = 2000; // год рождения
+
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+    public String patronymic; // поле для отчества
+    public String telephone; // номер телефон
+    public String passport; // номер паспорта
+    public String subscription; // наличие подписки
+    public FormDate birthday; // дата рождения
+
+}
